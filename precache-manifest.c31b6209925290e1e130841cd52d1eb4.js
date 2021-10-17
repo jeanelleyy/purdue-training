43d3e8516019f2ee874bc551b18a2f321f6a32b9
jeanelleyy <jeanelle.t@gmail.com>
2021-10-17 16:33:54 -0700

    Updates

---
+++ precache-manifest.c31b6209925290e1e130841cd52d1eb4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e85b7b252b0777c677af98120a9681b",
+    "url": "/purdue-training/index.html"
+  },
+  {
+    "revision": "15c5e0bae29b58107ebf",
+    "url": "/purdue-training/static/css/2.f1b50ea8.chunk.css"
+  },
+  {
+    "revision": "775c81a1ac4bb9e4fcfb",
+    "url": "/purdue-training/static/css/main.0fbf70f9.chunk.css"
+  },
+  {
+    "revision": "15c5e0bae29b58107ebf",
+    "url": "/purdue-training/static/js/2.11b7c7a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/purdue-training/static/js/2.11b7c7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775c81a1ac4bb9e4fcfb",
+    "url": "/purdue-training/static/js/main.4d9b2792.chunk.js"
+  },
+  {
+    "revision": "b19047132bac5b31c658",
+    "url": "/purdue-training/static/js/runtime-main.2bd3811f.js"
+  },
+  {
+    "revision": "15af9236c69d6ad263285860bf115cb0",
+    "url": "/purdue-training/static/media/B16_1_0.15af9236.png"
+  },
+  {
+    "revision": "60a9a0fc57c34c86f302426e799d8899",
+    "url": "/purdue-training/static/media/B1_1_2.60a9a0fc.png"
+  },
+  {
+    "revision": "0f0244d283618c5097c67b0ac42d0bf3",
+    "url": "/purdue-training/static/media/B2_34_2.0f0244d2.png"
+  },
+  {
+    "revision": "c59779ea35bfa77a7eb2d51ecf10aa97",
+    "url": "/purdue-training/static/media/B36_7_0.c59779ea.png"
+  },
+  {
+    "revision": "67e13e94df7485e2081f6822d6191f2c",
+    "url": "/purdue-training/static/media/B65_8_1.67e13e94.png"
+  },
+  {
+    "revision": "4271459652f66f4fd0ef46dd7a06f059",
+    "url": "/purdue-training/static/media/Objects.42714596.png"
+  },
+  {
+    "revision": "89643ea9847b8679aeb3c5a24b951655",
+    "url": "/purdue-training/static/media/blue.89643ea9.jpg"
+  },
+  {
+    "revision": "e4064255b5a8d94fd8bea0d1b028d289",
+    "url": "/purdue-training/static/media/green.e4064255.jpg"
+  },
+  {
+    "revision": "e368df29fca046705139f591d29e9bd7",
+    "url": "/purdue-training/static/media/red.e368df29.jpg"
+  },
+  {
+    "revision": "0fc15a3f97c7cc8cc5151d678b983403",
+    "url": "/purdue-training/static/media/yellow.0fc15a3f.jpg"
+  }
+]);
